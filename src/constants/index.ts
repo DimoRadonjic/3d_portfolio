d05e0fabe8e69fa715207d8b8f5de7d6a7d855a8
Dimitrije Radonjic <dimitrije.radonjic@acme.co.me>
2024-02-07 13:40:14 +0100

    Added new job to timeline

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,32 +18,33 @@ import {
   customShirtSite,
   imageGallery,
   metaverseSite,
-} from '../assets';
+  acmeLogo,
+} from "../assets";
 
 export const navLinks = [
   {
-    id: 'about',
-    title: 'About',
+    id: "about",
+    title: "About",
   },
   {
-    id: 'work',
-    title: 'Work',
+    id: "work",
+    title: "Work",
   },
   {
-    id: 'contact',
-    title: 'Contact',
+    id: "contact",
+    title: "Contact",
   },
 ];
 
 const services = [
   {
-    title: 'React Developer',
+    title: "React Developer",
     icon: web,
   },
-  // {
-  //   title: 'React Native Developer',
-  //   icon: mobile,
-  // },
+  {
+    title: "Frontend Developer",
+    icon: web,
+  },
   // {
   //   title: 'Backend Developer',
   //   icon: backend,
@@ -56,35 +57,35 @@ const services = [
 
 const technologies = [
   {
-    name: 'HTML 5',
+    name: "HTML 5",
     icon: html,
   },
   {
-    name: 'CSS 3',
+    name: "CSS 3",
     icon: css,
   },
   {
-    name: 'JavaScript',
+    name: "JavaScript",
     icon: javascript,
   },
   {
-    name: 'TypeScript',
+    name: "TypeScript",
     icon: typescript,
   },
   {
-    name: 'React JS',
+    name: "React JS",
     icon: reactjs,
   },
   {
-    name: 'Redux Toolkit',
+    name: "Redux Toolkit",
     icon: redux,
   },
   {
-    name: 'Tailwind CSS',
+    name: "Tailwind CSS",
     icon: tailwind,
   },
   {
-    name: 'SASS',
+    name: "SASS",
     icon: sass,
   },
   // {
@@ -92,15 +93,15 @@ const technologies = [
   //   icon: nodejs,
   // },
   {
-    name: 'MongoDB',
+    name: "MongoDB",
     icon: mongodb,
   },
   {
-    name: 'Three JS',
+    name: "Three JS",
     icon: threejs,
   },
   {
-    name: 'git',
+    name: "git",
     icon: git,
   },
   // {
@@ -115,31 +116,32 @@ const technologies = [
 
 const experiences = [
   {
-    title: 'React.js Developer',
-    company_name: 'ProfitApp',
+    title: "React Developer",
+    company_name: "ProfitApp",
     icon: profitappLogo,
-    iconBg: '#383E56',
-    date: 'Sept 2022 - Avg 2023',
+    iconBg: "#383E56",
+    date: "Sept 2022 - Avg 2023",
     points: [
-      'Developing and maintaining web applications using React.js',
-      'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-      'Implementing responsive design and ensuring cross-browser compatibility.',
-      'Participating in code reviews and providing constructive feedback to other developers.',
+      "Developing and maintaining web applications using React.js",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
-  // {
-  //   title: 'React Native Developer',
-  //   company_name: 'Tesla',
-  //   icon: tesla,
-  //   iconBg: '#E6DEDD',
-  //   date: 'Jan 2021 - Feb 2022',
-  //   points: [
-  //     'Developing and maintaining web applications using React.js and other related technologies.',
-  //     'Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.',
-  //     'Implementing responsive design and ensuring cross-browser compatibility.',
-  //     'Participating in code reviews and providing constructive feedback to other developers.',
-  //   ],
-  // },
+  {
+    title: "Frontend Developer",
+    company_name: "Acme",
+    icon: acmeLogo,
+    iconBg: "#383E56",
+    date: "Sept 2023 - *ongoing*",
+    points: [
+      "Developing a dynamic information system using React, Redux, Tailwind CSS, Axios, and Typescript.",
+      "Implementing a responsive table that dynamically adapts to backend changes for optimal user experience.",
+      "Enabling users to download data in Excel (XLSX), PDF, and DOCX formats for versatile document exports.",
+      "Facilitating seamless image uploads to enhance the comprehensive storage of employee data.",
+      "Incorporating user authentication",
+    ],
+  },
   // {
   //   title: 'Web Developer',
   //   company_name: 'Shopify',
@@ -171,101 +173,101 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      'I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.',
-    name: 'Sara Lee',
-    designation: 'CFO',
-    company: 'Acme Co',
-    image: 'https://randomuser.me/api/portraits/women/4.jpg',
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: 'Chris Brown',
-    designation: 'COO',
-    company: 'DEF Corp',
-    image: 'https://randomuser.me/api/portraits/men/5.jpg',
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: 'Lisa Wang',
-    designation: 'CTO',
-    company: '456 Enterprises',
-    image: 'https://randomuser.me/api/portraits/women/6.jpg',
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
 
 const projects = [
   {
-    name: 'Custom Shirt Creation ThreeJS Project',
+    name: "Custom Shirt Creation ThreeJS Project",
     description:
-      'Website where users can easily design personalized shirts. Choose colors, upload logos or images, and even experiment with AI-generated images to create a shirt that reflects your unique style.',
+      "Website where users can easily design personalized shirts. Choose colors, upload logos or images, and even experiment with AI-generated images to create a shirt that reflects your unique style.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'javascript',
-        color: 'green-text-gradient',
+        name: "javascript",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
     ],
     image: customShirtSite,
-    site_link: 'https://custom-shirt-creation.netlify.app',
-    source_code_link: 'https://github.com/DimoRadonjic/threejs_ai_project',
+    site_link: "https://custom-shirt-creation.netlify.app",
+    source_code_link: "https://github.com/DimoRadonjic/threejs_ai_project",
   },
   {
-    name: 'AI Images Showcase',
+    name: "AI Images Showcase",
     description:
       "Website where users can bring their ideas to life. Simply input your name and provide a prompt for the AI to work its magic. Watch as the AI generates stunning images tailored to your input. Share your creations with the community, allowing visitors to witness the art you've conjured and the prompts that inspired it.",
     tags: [
       {
-        name: 'react',
-        color: 'blue-text-gradient',
+        name: "react",
+        color: "blue-text-gradient",
       },
       {
-        name: 'mongodb',
-        color: 'green-text-gradient',
+        name: "mongodb",
+        color: "green-text-gradient",
       },
       {
-        name: 'netlify-serverless',
-        color: 'pink-text-gradient',
+        name: "netlify-serverless",
+        color: "pink-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
     ],
     image: imageGallery,
-    site_link: 'https://image-generation-gallery.netlify.app',
+    site_link: "https://image-generation-gallery.netlify.app",
     source_code_link:
-      'https://github.com/DimoRadonjic/project_ai_mern_image_generation',
+      "https://github.com/DimoRadonjic/project_ai_mern_image_generation",
   },
   {
-    name: 'Metaverse',
+    name: "Metaverse",
     description:
       "Discover METAVERUS: Your portal to an extraordinary metaverse experience. Immerse yourself in virtual realms that transcend reality, brought to life through VR technology. Select your desired destination, effortlessly enter, and unlock boundless exploration. Stay updated on new worlds, connect with friends, and gain insights into the metaverse's evolving impact. METAVERUS is your gateway to a captivating digital frontier where imagination knows no limits.",
     tags: [
       {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
+        name: "nextjs",
+        color: "blue-text-gradient",
       },
       {
-        name: 'typescript',
-        color: 'green-text-gradient',
+        name: "typescript",
+        color: "green-text-gradient",
       },
       {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
+        name: "tailwind",
+        color: "pink-text-gradient",
       },
     ],
     image: metaverseSite,
-    site_link: 'https://metaverse-next-ts.netlify.app',
-    source_code_link: 'https://github.com/DimoRadonjic/metaverse_next_ts',
+    site_link: "https://metaverse-next-ts.netlify.app",
+    source_code_link: "https://github.com/DimoRadonjic/metaverse_next_ts",
   },
 
   // {
